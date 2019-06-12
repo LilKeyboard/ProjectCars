8c2c2a7b03b0b6fbb22958bb5474b2b78fbddd73
Wiktor Balcerzak <wbalcerzak97@o2.pl>
2019-06-12 23:54:43 +0200

    Add OpinionController

---
+++ ProjectCars/Controllers/OpinionController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ProjectCars.Models;
+
+namespace ProjectCars.Controllers
+{
+    public class OpinionController : Controller
+    {
+        private readonly IOpinionRepository _opinionRepository;
+
+        public OpinionController(IOpinionRepository opinionRepository)
+        {
+            _opinionRepository = opinionRepository;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
